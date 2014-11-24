c16be495dfba8fa35452b9bb5d997d02f2b2db17
Russell Reventar <mac>
2014-11-24 12:12:23 -0500

    sql

--- Mini_Bizi.sql
+++ Mini_Bizi.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost:8889
--- Generation Time: Oct 25, 2014 at 03:01 AM
+-- Generation Time: Nov 24, 2014 at 06:11 PM
 -- Server version: 5.5.38
 -- PHP Version: 5.6.2
 
@@ -17,6 +17,33 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `Businesses`
+--
+
+CREATE TABLE `Businesses` (
+`BusinessID` int(11) unsigned NOT NULL,
+  `OwnerID` int(11) unsigned NOT NULL,
+  `BusinessName` varchar(128) DEFAULT NULL,
+  `Address` varchar(128) DEFAULT NULL,
+  `City` varchar(128) DEFAULT NULL,
+  `Country` varchar(128) DEFAULT NULL,
+  `PostalCode` varchar(20) DEFAULT NULL,
+  `Phone` int(20) DEFAULT NULL,
+  `DateOpened` date DEFAULT NULL,
+  `ProfileImage` varchar(128) DEFAULT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `Businesses`
+--
+
+INSERT INTO `Businesses` (`BusinessID`, `OwnerID`, `BusinessName`, `Address`, `City`, `Country`, `PostalCode`, `Phone`, `DateOpened`, `ProfileImage`) VALUES
+(3, 1, 'Lady Christines Baby Back Ribs', '123 Test Street', 'Lipa', 'Philippines', 'L26 123', 1231231233, NULL, NULL),
+(4, 1, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `Users`
 --
 
@@ -25,21 +52,29 @@ CREATE TABLE `Users` (
   `Username` text NOT NULL,
   `Password` text NOT NULL,
   `Active` int(11) NOT NULL DEFAULT '0',
-  `Email` varchar(128) NOT NULL
+  `Email` varchar(128) NOT NULL,
+  `FirstName` varchar(128) DEFAULT NULL,
+  `LastName` varchar(128) DEFAULT NULL
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `Users`
 --
 
-INSERT INTO `Users` (`UID`, `Username`, `Password`, `Active`, `Email`) VALUES
-(1, 'joe', 'pass123', 1, ''),
-(2, 'bob', 'pass123', 0, ''),
-(3, 'russ', 'pass123', 0, '');
+INSERT INTO `Users` (`UID`, `Username`, `Password`, `Active`, `Email`, `FirstName`, `LastName`) VALUES
+(1, 'bill', 'pass123', 1, '', 'Bill', 'Gates'),
+(2, 'bob', 'pass123', 0, '', NULL, NULL),
+(3, 'russ', 'pass123', 0, '', NULL, NULL);
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `Businesses`
+--
+ALTER TABLE `Businesses`
+ ADD PRIMARY KEY (`BusinessID`);
 
 --
 -- Indexes for table `Users`
@@ -51,6 +86,11 @@ ALTER TABLE `Users`
 -- AUTO_INCREMENT for dumped tables
 --
 
+--
+-- AUTO_INCREMENT for table `Businesses`
+--
+ALTER TABLE `Businesses`
+MODIFY `BusinessID` int(11) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
 --
 -- AUTO_INCREMENT for table `Users`
 --
