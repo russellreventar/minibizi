f027f006b2e91a979b818b5d89ab451bff0dc1f2
Russell Reventar <mac>
2014-11-28 17:26:46 -0500

    sql

--- Mini_Bizi.sql
+++ Mini_Bizi.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost:8889
--- Generation Time: Nov 28, 2014 at 05:20 AM
+-- Generation Time: Nov 28, 2014 at 11:25 PM
 -- Server version: 5.5.38
 -- PHP Version: 5.6.2
 
@@ -29,7 +29,8 @@ CREATE TABLE `Businesses` (
   `Country` varchar(128) DEFAULT NULL,
   `PostalCode` varchar(20) DEFAULT NULL,
   `Phone` int(20) DEFAULT NULL,
-  `DateOpened` date DEFAULT NULL,
+  `StockName` varchar(11) DEFAULT NULL,
+  `DateOpened` varchar(128) DEFAULT NULL,
   `ProfileImage` varchar(128) DEFAULT NULL
 ) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
 
@@ -37,9 +38,9 @@ CREATE TABLE `Businesses` (
 -- Dumping data for table `Businesses`
 --
 
-INSERT INTO `Businesses` (`BusinessID`, `UserID`, `BusinessName`, `Address`, `City`, `Country`, `PostalCode`, `Phone`, `DateOpened`, `ProfileImage`) VALUES
-(3, 1, 'Lady Christines Baby Back Ribs', '123 Test Street', 'Lipa', 'Philippines', 'L26 123', 1231231233, '1992-10-15', NULL),
-(4, 1, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+INSERT INTO `Businesses` (`BusinessID`, `UserID`, `BusinessName`, `Address`, `City`, `Country`, `PostalCode`, `Phone`, `StockName`, `DateOpened`, `ProfileImage`) VALUES
+(3, 1, 'Lady Christines Baby Back Ribs', '123 Test Street', 'Lipa', 'Philippines', 'L26 123', 1231231234, 'GOOG', 'October 15, 1992', NULL),
+(4, 1, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
 
 -- --------------------------------------------------------
 
